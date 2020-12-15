1fe2364494acb4e053a28a943671b69c2334a0b7
Dragos Gaftoneanu <43792974+dragosgaftoneanu@users.noreply.github.com>
2020-12-15 13:32:20 +0200

    Day 15

---
+++ day-15/p1.php
@@ -0,0 +1,24 @@
+<?php
+$input =<<<START
+8,11,0,19,1,2
+START;
+
+preg_match_all('/[\d]+/', $input, $numbers);
+$numbers = array_flip(array_merge(array(""), $numbers[0]));
+
+for ($i=count($numbers);$i<=2020;$i++)
+{
+	if(empty($lastStep))
+		$number = 0;
+	else
+		$number = $i - 1 - $lastStep;
+	
+	if(empty($numbers[$number]))
+		$lastStep = 0;
+	else
+		$lastStep = $numbers[$number];
+	
+	$numbers[$number] = $i;
+}
+
+echo "Number: " . $number . "\n";
